049b8cd7f14dfd53209311f052ddb81d1cefd343
ikromrixsiyev1919-ctrl <ikromrixsiyev1919@gmail.com>
2026-06-16 17:38:30 +0500

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,6 @@
+const mevalar = ['olma', 'anor', 'uzum', 'ananas', 'anjir', 'ambirella'];
+
+const aBilanBoshlanadigan = mevalar.filter(el => 
+    el.toUpperCase().toUpperCase.startsWith('A')
+);
+console.log('abilanboshlanadigan');
---
+++ rrr.js
@@ -0,0 +1,2 @@
+const U1 = 'rasmlar/koylak-oq.jpeg';   // → o'z rasmingiz yo'li
+const U2 = 'rasmlar/tufli-qora.jpeg';  // → o'z rasmingiz yo'li
